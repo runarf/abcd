20b97acf44684460b4ca2651031ae2994cc5457a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-09 12:14:37 +0000

    Deploy runarf/abcd to github.com/runarf/abcd.git:gh-pages

---
+++ precache-manifest.de2284b957fcb3e1da8e9cc32253af75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b19e8c21cb622eaeccc64e2f138b30e6",
+    "url": "/abcd/index.html"
+  },
+  {
+    "revision": "dacca8d88a818cb1c791",
+    "url": "/abcd/static/css/2.717d6787.chunk.css"
+  },
+  {
+    "revision": "e096091e658aa330315b",
+    "url": "/abcd/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dacca8d88a818cb1c791",
+    "url": "/abcd/static/js/2.c2e8496b.chunk.js"
+  },
+  {
+    "revision": "e5a82244df93883024cde4087cb6c00a",
+    "url": "/abcd/static/js/2.c2e8496b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e096091e658aa330315b",
+    "url": "/abcd/static/js/main.33cecc3b.chunk.js"
+  },
+  {
+    "revision": "edb9ac0de1cf6dddf64f",
+    "url": "/abcd/static/js/runtime-main.b5418576.js"
+  }
+]);
